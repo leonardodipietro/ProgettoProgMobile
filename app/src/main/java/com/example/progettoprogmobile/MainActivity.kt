0b9b19706fa91923cbd0067fc38852931c27413b
Giulia Fronzi <S1097903@studenti.unvpm.it>
2023-09-08 15:08:29 +0200

    Ho eliminato il secondo login

--- app/src/main/java/com/example/progettoprogmobile/MainActivity.kt
+++ app/src/main/java/com/example/progettoprogmobile/MainActivity.kt
@@ -2,26 +2,15 @@ package com.example.progettoprogmobile
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.progettoprogmobile.viewModel.FirebaseAuthViewModel
 import com.google.firebase.FirebaseApp
-import com.firebase.ui.auth.AuthUI
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel = FirebaseAuthViewModel()
-    private lateinit var auth: FirebaseAuth
-    private lateinit var button: Button
-    private lateinit var textView: TextView
-    private var user: FirebaseUser? = null //per evitare il tipo mismatch error
-
-    // Initialize the FirebaseUI Widget using Firebase
-    var ui: AuthUI = AuthUI.getInstance()
 
     private lateinit var firebaseviewModel: FirebaseAuthViewModel
     private val signInLauncher = registerForActivityResult(
@@ -35,30 +24,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel.checkAuthenticationStatus()
-
-        auth = FirebaseAuth.getInstance()
-        button = findViewById(R.id.logout)
-        textView = findViewById(R.id.user_details)
-        user = auth.currentUser
-        if (user == null) {
-            // L'utente non è autenticato, reindirizza alla schermata di login
-            val intent = Intent(applicationContext, Login::class.java)
-            startActivity(intent)
-            finish()
-        } else {
-            textView.text = user?.email //per accedere alle proprietà di user in modo sicuro, poiché può essere nullo
-
-            // Configura il pulsante per eseguire il logout
-            button.setOnClickListener {
-                // Esegui il logout dall'account corrente
-                FirebaseAuth.getInstance().signOut()
-
-                // Reindirizza nuovamente alla schermata di login dopo il logout
-                val intent = Intent(applicationContext, Login::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
 
         // Inizializza la ViewModel
         firebaseviewModel = ViewModelProvider(this)[FirebaseAuthViewModel::class.java]
